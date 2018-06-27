97da123de54fd59298a3bac1a109a3b4584c0fb8
yamen_rbd <yamenrbd@gmail.com>
2018-06-27 19:19:08 +0300

    new

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.android.miwok;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,22 +16,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openFamilyList(View view){
-        Intent intent = new Intent(this , FamilyMemebrActivity.class);
-        startActivity(intent);
-    }
-    public void openColorsList(View view){
-        Intent intent = new Intent(this , ColorActivity.class);
-        startActivity(intent);
-    }
-    public void openPhrasesList(View view){
-        Intent intent = new Intent(this , PhrasesActivity.class);
-        startActivity(intent);
-    }
-    public void openNumberList(View view){
-        Intent intent = new Intent(this , NumberActivity.class);
-        startActivity(intent);
+        // create variable for each text catagory group
+        TextView number = (TextView) findViewById(R.id.numbers);
+        TextView color = (TextView) findViewById(R.id.colors);
+        TextView family = (TextView) findViewById(R.id.family);
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+
+        //create onClickListener for number textView
+        number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create intente to new activity number when textView number have been clicked
+                Intent intent = new Intent(MainActivity.this, NumberActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //create onClickListener for color textView
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create intente to new activity number when textView color have been clicked
+                Intent intent = new Intent(MainActivity.this, ColorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //create onClickListener for family textView
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create intente to new activity number when textView family have been clicked
+                Intent intent = new Intent(MainActivity.this, FamilyMemebrActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //create onClickListener for phrases textView
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //create intente to new activity number when textView phrases have been clicked
+                Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        String [] numberName = new String[10];
+        numberName[0]= "one";
     }
 }
--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -9,5 +9,17 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+        String [] word = new String[10];
+        word[0]= "one";
+        word[1]= "two";
+        word[2]= "three";
+        word[3]= "four";
+        word[4]= "five";
+        word[5]= "six";
+        word[6]= "seven";
+        word[7]= "eight";
+        word[8]= "nine";
+        word[0]= "ten";
     }
 }
