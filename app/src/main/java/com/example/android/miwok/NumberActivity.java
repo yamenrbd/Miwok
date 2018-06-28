726943ae617ec61c25c2af171e0699b0dcafb5a0
yamen_rbd <yamenrbd@gmail.com>
2018-06-28 11:19:05 +0300

    28/6/2018

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -10,16 +13,18 @@ public class NumberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
 
-        String [] word = new String[10];
-        word[0]= "one";
-        word[1]= "two";
-        word[2]= "three";
-        word[3]= "four";
-        word[4]= "five";
-        word[5]= "six";
-        word[6]= "seven";
-        word[7]= "eight";
-        word[8]= "nine";
-        word[0]= "ten";
+        // create arrayList that can hold name of numbers from 1 to 10
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("sex");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
     }
 }
